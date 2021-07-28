121a6cc04badbc4587235fe4308a320dfe3a4815
Alexei Lifanov <alexeilifanov@alexeis-air.myfiosgateway.com>
2021-07-27 22:14:47 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        res.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return res
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num += -1
+    }
+}
